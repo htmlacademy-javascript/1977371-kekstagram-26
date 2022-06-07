a8e2c8436b45edb08eef1ed8a7baed73ecb0ccae
Евгений Калинин <97776034+EvgeniyDobrov@users.noreply.github.com>
2022-06-07 23:09:17 +0600

    сделал первую функцию

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
-let number = Math.ceil(Math.random());
-alert(number);
+// eslint-disable-next-line no-unused-vars
+function getRandomIntInc(min, max) {
+  if (min >= 0 && min < max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  else if (min < 0 && min > max) { console.log('Условие не верно')}
+}
+// console.log(getRandomInc(1, 10));
