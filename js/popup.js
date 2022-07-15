6658e6174d9515f11bd913997f4f57fa46edf70d
Евгений Калинин <97776034+EvgeniyDobrov@users.noreply.github.com>
2022-07-15 18:30:27 +0300

    исправили ошибки

--- js/popup.js
+++ js/popup.js
@@ -1,23 +1,28 @@
 import { createFotos } from './data.js';
 
+// const userDialog = document.querySelector('.big-picture');
+// userDialog.classList.remove('hidden');
 
-const userDialog = document.querySelector('.big-picture');
-userDialog.classList.remove('hidden');
-
-const similarListFoto = userDialog.querySelector('.pictures');
+const similarListFoto = document.querySelector('.pictures');
 const similarCreateFotos = document.querySelector('#picture').content.querySelector('.picture');
 
 const similarFotos = createFotos(25);
 
 const similarListFragment = document.createDocumentFragment();
 
-similarFotos.forEach(({likes, comments}) => {
-  const profileFoto = similarCreateFotos.cloneNode(true);
-  // eslint-disable-next-line no-undef
-  profileFoto.querySelector('.picture__likes').textContent = profile.likes;
-  // eslint-disable-next-line no-undef
-  profileFoto.querySelector('.picture__comments').textContent = profile.comments;
-  similarListFoto.appendChild(profileFoto);
-});
 
-similarListFoto.appendChild(similarListFragment);
+const renderFotos = function () {
+  similarFotos.forEach(({likes, comments, url }) => {
+    const profileFoto = similarCreateFotos.cloneNode(true);
+
+    profileFoto.querySelector('.picture__likes').textContent = likes;
+    profileFoto.querySelector('.picture__comments').textContent = comments.length;
+    profileFoto.querySelector('.picture__img').src = url;
+    similarListFoto.appendChild(profileFoto);
+  });
+
+  similarListFoto.appendChild(similarListFragment);
+  return similarListFoto;
+};
+
+export {renderFotos};
